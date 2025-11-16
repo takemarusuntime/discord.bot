92e63633b8d428473ee3e1c8326ea705e56592a3
takemarusuntime <haishin.vtuber.twi@gmail.com>
2025-11-17 05:13:26 +0900

    update

--- keep_alive.py
+++ keep_alive.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask
-from threading import Thread
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Alive"
-
-@app.route("/healthz")
-def healthz():
-    return "OK", 200
-
-def run():
-    port = int(os.environ.get("PORT", 8080))
-    print(f"### Flask starting on port {port} ###")
-    app.run(host="0.0.0.0", port=port)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
-=======
 # keep_alive.py
 from flask import Flask
 from threading import Thread
@@ -47,4 +22,3 @@ def keep_alive():
     t = Thread(target=run)
     t.daemon = True
     t.start()
->>>>>>> cce80afcf54b9784660ed8cb4fab1f1d26e5680f
